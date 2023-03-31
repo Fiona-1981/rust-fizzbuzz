5c4421ea20956ab95f5e4ebe5727344b7d1aa062
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-03-31 18:02:19 +0100

    Let the fun begin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("FizzBuzz coming soon!");
+    let num = 1..=40;
+
+    if num % 15 == 0 {
+        println!("FizzBuzz");
+    } else if num % 3 == 0 {
+        println!("Fizz");
+    } else if num % 5 == 0 {
+        println!("Buzz")
+    } else {
+        return num;
+    }
 }
